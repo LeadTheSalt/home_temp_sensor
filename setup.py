809d6c605a690f78c118759c671c2ef067fa1e7e
LeadtheSalt <leadthesalt.soc@gmail.com>
2019-10-20 18:22:16 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="home_temp_sensor",
-    version='0.0.2',
+    version='0.0.3',
     author="LeadTheSalt",
     author_email="leadthesalt.soc@gmail.com",
     description="My home tempearture sensore on a RaspberryPi",
