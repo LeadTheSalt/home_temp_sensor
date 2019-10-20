5849ad57eca97b685e63c3c806150a725fca2947
LeadtheSalt <leadthesalt.soc@gmail.com>
2019-10-20 18:17:09 +0200

    addind tests 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="home_temp_sensor",
-    version='0.0.1',
+    version='0.0.2',
     author="LeadTheSalt",
     author_email="leadthesalt.soc@gmail.com",
     description="My home tempearture sensore on a RaspberryPi",
